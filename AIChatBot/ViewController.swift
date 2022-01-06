8d95e34ae32fc09b17b8b830172180bc6349151e
Teagan Rett <teagan.rett@gmail.com>
2022-01-06 09:12:20 -0600

    Initial Commit

---
+++ AIChatBot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AIChatBot
+//
+//  Created by TEAGAN RETT on 1/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
