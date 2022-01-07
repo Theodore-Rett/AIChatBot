5a875e234aea7a7d9f630aa8861e2ea188501886
PaigeKeller <Paige Keller>
2022-01-07 09:19:40 -0600

    outlet

--- AIChatBot/ViewController.swift
+++ AIChatBot/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var responseOutlet: UITextField!
+    var wordArray: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    func filterResponse(response: String) {
+      var temp = response
+        
+        if let wordInd = temp.firstIndex(of: " ") {
+        var wordIndAfter = temp.index(after: wordInd)
+            wordArray.append(temp[...wordIndAfter])
+            temp = temp[wordInd...]
+        } else { //let newStr = str[..<index]
+            print("no spaces in string")
+        }
+      
+        
+    }
+    
 
 }
 
