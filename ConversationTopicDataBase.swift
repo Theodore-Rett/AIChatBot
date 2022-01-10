b215783c1a868ac3d88f115d0eb29b3c4c79cb53
Teagan Rett <teagan.rett@gmail.com>
2022-01-10 10:21:09 -0600

    topic database

---
+++ ConversationTopicDataBase.swift
@@ -0,0 +1,29 @@
+//
+//  ConversationTopicDataBase.swift
+//  AIChatBot
+//
+//  Created by TEAGAN RETT on 1/10/22.
+//
+
+import Foundation
+
+import Foundation
+public class ConversationTopicDataBase{
+    //sports
+    var sports = ["BASKETBALL", "BASEBALL", "SOFTBALL", "VOLLEYBALL", "HOCKEY", "FOOTBALL"]
+    
+    var basketball = ["BASKETBALL", "COURT", "ORANGE", "B-BALL", "GUARD", "FORWARD", "SHOOTING", "NET", "SLAM DUNK", "HOOP", "BACKBOARD", "DRIBBLE", "NBA", "WNBA", "BULLS", "LAKERS", "WARRIORS", "NETS", "SUNS", "RAPTORS", "CELTICS", "KNICKS", "HEAT", "MAVERICKS", "BUCKS", "CAVALIERS", "JAZZ", "76ERS", "SPURS", "CLIPPERS", "NUGGETS", "GRIZZLIES", "HORNETS", "WIZARDS", "ROCKETS", "BLAZERS", "KINGS", "TIMBERWOLVES", "PACERS", "HAWKS", "PELICANS", "PISTONS", "CITY THUNDER", "MAGIC", "MYSTICS", "SKY", "ACES", "STORM", "LYNX", "LIBERTY", "MERCURY", "SPARKS", "DREAM", "WINGS", "FEVER", "COMETS", "STARS", "STING", "MONARCHS", "SHOCKS", "ROCKERS", "STARZZ", "SOL", "FIRE", "MIRACLE", "RAGE"]
+    
+    //school
+    
+    //tv
+    
+    //computer science
+    
+    //about your day
+    
+    init(){
+        
+    }
+    
+}
