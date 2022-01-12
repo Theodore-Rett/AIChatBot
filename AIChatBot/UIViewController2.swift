e24fc12862cf563469d3026005b80697e053e20b
PaigeKeller <Paige Keller>
2022-01-12 09:50:19 -0600

    select and deselect interests

--- AIChatBot/UIViewController2.swift
+++ AIChatBot/UIViewController2.swift
@@ -10,7 +10,7 @@ import UIKit
 class UIViewController2: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var stack1: UICollectionView!
-    let images: [UIImage] = [UIImage(named: "bball")!, UIImage(named: "baseball")!, UIImage(named: "proWSoccer")!, UIImage(named: "nfl")!, UIImage(named: "track")!, UIImage(named: "pool")!, UIImage(named: "math")!, UIImage(named: "compSci")!, UIImage(named: "books")!, UIImage(named: "netflix")!, UIImage(named: "chem")!, UIImage(named: "beach")!, UIImage(named: "mountains")!, UIImage(named: "desert")! ]
+    let images: [UIImage] = [UIImage(named: "bball")!, UIImage(named: "baseball")!,UIImage(named: "hockey")!, UIImage(named: "nfl")!, UIImage(named: "track")!,  UIImage(named: "proWSoccer")!, UIImage(named: "pool")!, UIImage(named: "volleyball")!, UIImage(named: "math")!, UIImage(named: "compSci")!, UIImage(named: "books")!, UIImage(named: "netflix")!, UIImage(named: "beach")!, UIImage(named: "mountains")!, UIImage(named: "desert")! ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +18,28 @@ class UIViewController2: UIViewController, UICollectionViewDelegate, UICollectio
         stack1.delegate = self
         stack1.dataSource = self
     }
+    
+    
+    
+    @IBAction func finishAction(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = stack1.cellForItem(at: indexPath)! as! CustomCell
+        if cell.myBool == false {
+        cell.backgroundColor = UIColor.green
+            cell.myBool = true
+        } else {
+            cell.backgroundColor = UIColor.clear
+                cell.myBool = false
+        }
+    }
+    
+    
+    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         images.count
